5cd2ce684563eb4656f8fe01aef7da3c373d5ec0
tyler-mairose-sp <tyler.mairose@sailpoint.com>
2023-08-01 14:05:08 -0400

    Updates to project from writing guide

--- src/test/java/sailpoint/UsernameGeneratorTest.java
+++ src/test/java/sailpoint/UsernameGeneratorTest.java
@@ -5,18 +5,14 @@ import bsh.Interpreter;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
-import sailpoint.connector.webservices.Endpoint;
-import sailpoint.connector.webservices.WebServicesClient;
-import sailpoint.object.*;
+import sailpoint.object.Application;
+import sailpoint.object.Identity;
 import sailpoint.ps.utils.RuleXmlUtils;
-import sailpoint.rule.Account;
-import sailpoint.rule.ManagedAttributeDetails;
 import sailpoint.server.IdnRuleUtil;
 import sailpoint.tools.GeneralException;
 
-import sailpoint.object.ProvisioningPlan;
-import sailpoint.object.ProvisioningPlan.AccountRequest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -36,25 +32,6 @@ public class UsernameGeneratorTest {
         Application application = mock(Application.class);
         when(application.getName()).thenReturn("applicationName");
 
-        ProvisioningPlan plan = mock(ProvisioningPlan.class);
-        Filter filter = mock(Filter.class);
-
-        AccountRequest accountRequest = mock(AccountRequest.class);
-        when(accountRequest.getOp()).thenReturn(ProvisioningPlan.ObjectOperation.Disable);
-
-        plan.getAccountRequests();
-//        ManagedAttribute managedAttribute = mock(ManagedAttribute.class);
-//        AttributeDefinition attributeDefinition = mock(AttributeDefinition.class);
-//        ManagedAttributeDetails managedAttributeDetails = mock(ManagedAttributeDetails.class);
-//        Link link = mock(Link.class);
-//        QueryOptions qo = mock(QueryOptions.class);
-//        Account account = mock(Account.class);
-//        Bundle bundle = mock(Bundle.class);
-//        Endpoint endpoint = mock(Endpoint.class);
-//        WebServicesClient webServicesClient = mock(WebServicesClient.class);
-//        Field field = mock(Field.class);
-//        Schema schema = mock(Schema.class);
-
         Identity identity = mock(Identity.class);
         when(identity.getFirstname()).thenReturn("Tyler");
         when(identity.getLastname()).thenReturn("Smith");
@@ -69,10 +46,10 @@ public class UsernameGeneratorTest {
         String source = RuleXmlUtils.readRuleSourceFromFilePath(RULE_FILENAME);
         result = (String) i.eval(source);
 
+        assertNotNull(result);
+        assertEquals(result, "tyler.s");
+
         log.info("Beanshell script returned: " + result);
-
-
-
 
     }
 }
